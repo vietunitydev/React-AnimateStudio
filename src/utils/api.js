4fa508b669d2a266b89592de3dca1ef9582c6864
viet <vietunitydev2222@gmail.com>
2025-09-30 16:20:32 +0700

    - change basebackend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // utils/api.js
 
 // API Base URL - thay đổi theo server của bạn
-const API_BASE_URL = 'http://localhost:4999/api';
+const API_BASE_URL = import.meta.env.API_BASE_URL;
 
 /**
  * Generic API call function
